c1683158e55b3736c442e2163b7bab8fc441738c
wxb0418 <418909149@qq.com>
2019-03-31 20:51:47 +0800

    商品提供可以启动

--- aigou_06_product_parent/aigou_product_service_8011/src/main/java/cn/itsource/ProductServiceApplication.java
+++ aigou_06_product_parent/aigou_product_service_8011/src/main/java/cn/itsource/ProductServiceApplication.java
@@ -1,11 +1,13 @@
 package cn.itsource;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan("cn.itsource.mapper")
 public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class);
